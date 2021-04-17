c7a036e88404921788e7c6ac12fe06f9d4df83dc
hajujang <haju.jang37@gmail.com>
2021-04-17 12:28:28 -0500

    initial commit

--- noober.js
+++ noober.js
@@ -14,4 +14,36 @@ document.addEventListener('DOMContentLoaded', async function() {
   // Write the recipe (algorithm) in the comments. Then, write the code.
   
   // 🔥 YOUR CODE ENDS HERE 🔥
+  // Passenger detials, pickup location, and dropoff location
+  let passengerFirstName = ride.passengerDetails.first
+  let passengerLastName = ride.passengerDetails.last
+  let passengerPhoneNumber = ride.passengerDetails.phoneNumber
+
+  let pickupAddress = ride.pickupLocation.address
+  let pickupCity = ride.pickupLocation.city 
+  let pickupState = ride.pickupLocation.state 
+  let pickupZip = ride.pickupLocation.zip 
+
+  let dropoffAddress = ride.dropoffLocation.address
+  let dropoffCity = ride.dropoffLocation.city 
+  let dropoffState = ride.dropoffLocation.state 
+  let dropoffZip = ride.dropoffLocation.zip 
+
+  // Noober Purple or not
+  let levelOfService 
+  if (ride.purpleRequested==true){
+    levelOfService = `Noober Purple Passegner`
+  } else if (ride.numberOfPassengers > 3){
+    levelOfService = 'Noober XL Passenger'
+  } else {
+    levelOfService = 'Noober X Passenger'
+  }
+  // console.log(levelOfService) 
+  // console.log(ride.purpleRequested)
+  // console.log(ride.numberOfPassengers)
+
+  // A human-readable format
+  console.log(`${levelOfService}: ${passengerFirstName} ${passengerLastName} - ${passengerPhoneNumber}. Pickup at ${pickupAddress}, ${pickupCity}, ${pickupState} ${pickupZip}. Drop-off at ${dropoffAddress}, ${dropoffCity}, ${dropoffState} ${dropoffZip}.`)
+
+
 })
